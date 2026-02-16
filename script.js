0d66bf8cebaeae4d53c8359c793b7ba6ba857f55
vxntality <codedrycucumber@gmail.com>
2026-02-16 21:28:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("bunnyBtn").addEventListener("click", function() {
+    window.location.href = "https://image2url.com/r2/default/videos/1771271255539-6e2c1370-85c0-4dc0-8769-7829a5b772c5.mp4";
+});

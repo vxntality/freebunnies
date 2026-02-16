e1328057b94f70a483f5af987bd302940e18dbaf
vxntality <codedrycucumber@gmail.com>
2026-02-16 22:19:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 // ===== EDIT THESE SETTINGS =====
 const intervalSeconds = 2;
-const url = "https://image2url.com/r2/default/videos/1771271255539-6e2c1370-85c0-4dc0-8769-7829a5b772c5.mp4";
+const urls = [
+    "https://image2url.com/r2/default/videos/1771271255539-6e2c1370-85c0-4dc0-8769-7829a5b772c5.mp4",
+    "https://image2url.com/r2/default/videos/1771271255539-6e2c1370-85c0-4dc0-8769-7829a5b772c5.mp4s",
+    "https://image2url.com/r2/default/videos/1771271255539-6e2c1370-85c0-4dc0-8769-7829a5b772c5.mp4",
+    "https://image2url.com/r2/default/videos/1771271255539-6e2c1370-85c0-4dc0-8769-7829a5b772c5.mp4"
+];
 // ===============================
 
+let currentIndex = 0;
 let intervalId = null;
 
 document.getElementById("bunnyBtn").addEventListener("click", () => {
     if (intervalId !== null) return; // run once
 
     intervalId = setInterval(() => {
-        // Each call opens a NEW browser window with ONE tab
-        window.open(url, "_blank", "popup=yes");
+        // Open new browser window with ONE tab
+        window.open(urls[currentIndex], "_blank", "popup=yes");
+
+        // Move to next URL (loop back to start)
+        currentIndex = (currentIndex + 1) % urls.length;
     }, intervalSeconds * 1000);
 });

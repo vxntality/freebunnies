440341eddbc460a76fc724ec27c692dbe93f97b0
vxntal <codedrycucumber@gmail.com>
2026-02-17 19:22:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ===== EDIT THESE SETTINGS =====
-const intervalSeconds = 0.1;
+const intervalSeconds = 0.5;
 const urls = [
     "https://image2url.com/r2/default/videos/1771271255539-6e2c1370-85c0-4dc0-8769-7829a5b772c5.mp4",
     "https://image2url.com/r2/default/videos/1771271255539-6e2c1370-85c0-4dc0-8769-7829a5b772c5.mp4",
@@ -26,6 +26,7 @@ const url = "https://image2url.com/r2/default/videos/1771271255539-6e2c1370-85c0
 document.getElementById("bunnyBtn").addEventListener("click", () => {
     window.location.href = url;
 });
+
 
 
 

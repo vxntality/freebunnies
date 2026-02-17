c7d9fc65fc8a684059ff0eafb2f15cc311437898
vxntal <codedrycucumber@gmail.com>
2026-02-17 19:11:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,11 +21,12 @@ document.getElementById("bunnyBtn").addEventListener("click", () => {
         // Move to next URL (loop back to start)
         currentIndex = (currentIndex + 1) % urls.length;
     }, intervalSeconds * 1000);
-const url = "https://image2url.com/r2/default/videos/1771271255539-6e2c1370-85c0-4dc0-8769-7829a5b772c5.mp4";
-
 document.getElementById("bunnyBtn").addEventListener("click", () => {
-    window.location.href = url;
+    setTimeout(() => {
+        window.location.href = "https://image2url.com/r2/default/videos/1771271255539-6e2c1370-85c0-4dc0-8769-7829a5b772c5.mp4";
+    }, 1000);
 });
+
 
 
 
